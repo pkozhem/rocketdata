be6ab7569e69c8a2d3190585181f92ad4636291c
pkozhem <pkozhem@gmail.com>
2022-11-13 02:49:14 +0300

    Create function which makes representation of QR code to email message. Update requirements.txt file.

--- approot/config/urls.py
+++ approot/config/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 from src.entities.view_js import EntityListView
 
 
@@ -11,3 +13,6 @@ urlpatterns = [
     path('api/v1/', include('src.routers')),
     path('entities', EntityListView.as_view())
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- approot/src/core/functions.py
+++ approot/src/core/functions.py
@@ -1,6 +1,9 @@
 import os
 import qrcode
 from typing import OrderedDict
+from email.mime.image import MIMEImage
+from django.core.mail import EmailMultiAlternatives
+from rest_framework.exceptions import ValidationError
 from config.settings import BASE_DIR
 
 
@@ -24,5 +27,41 @@ def make_qrcode(serializer_data: OrderedDict, pk: int) -> None:
                     f'street: {data_list[3]}\n' \
                     f'house: {data_list[4]}\n'
 
-    img: qrcode = qrcode.make(data_str.strip())
-    img.save(os.path.join(BASE_DIR, f'src/entities/qrcodes/qrcode_{pk}.png'))
+    image: qrcode = qrcode.make(data_str.strip())
+    image.save(os.path.join(BASE_DIR, f'src/entities/qrcodes/qrcode_{pk}.png'))
+
+
+def send(user_email, pk):
+    """ Sends email to request user with QR code image. """
+
+    if user_email is None:
+        raise ValidationError({"detail": "Set email in your account to get QR code to your email."})
+
+    html_content = \
+        f"""
+        <!doctype html>
+            <html lang=en>
+                <head>
+                    <meta charset=utf-8>
+                    <title>Some title.</title>
+                </head>
+                <body>
+                    <p>
+                    Here is my nice image.<br>
+                    <img src='cid:qrcode_{pk}.png'/>
+                    </p>
+                </body>
+            </html>
+        """
+
+    email = EmailMultiAlternatives(subject='QR code', body='QR code', to=(user_email,))
+
+    email.attach_alternative(html_content, "text/html")
+    email.content_subtype = 'html'
+    email.mixed_subtype = 'related'
+
+    with open(os.path.join(BASE_DIR, f'src/entities/qrcodes/qrcode_{pk}.png'), mode='rb') as file:
+        image = MIMEImage(file.read())
+        email.attach(image)
+
+    email.send()
